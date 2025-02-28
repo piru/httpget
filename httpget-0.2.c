a54171f522c5748bbee13fac3f167a7100216d71
Daniel Stenberg <daniel@haxx.se>
2025-02-28 15:58:53 +0100

    httpget-0.2: cleanups
    - the accidental linte wraps on line 14 and 16 were corrected. They
      mostly happened due to email line wrapping.
    
    - I removed the outcommented lines for #include files as I believe they
      were there there only for the purposes of making the code build on
      Windows for the person who got his email.
    
    - I removed the winsock.h include file for the same reason, this code was
      written to run on Unix initially.

--- httpget-0.2.c
+++ httpget-0.2.c
@@ -4,16 +4,14 @@
  * Compile with: gcc httpget.c -o httpget
  *           or: cc httpget.c -o httpget
  * Usage: httpget <host> <port> <remote_file> <local_file>
- *      Example: httpget 143.54.10.6 80 /info_logo.gif teste.gif 
- * TODO: implement URLs. 
+ *      Example: httpget 143.54.10.6 80 /info_logo.gif teste.gif
+ * TODO: implement URLs.
  *
  * Rafael Sagula - sagula@inf.ufrgs.br
  *                 http://www.inf.ufrgs.br/~sagula
- * 
- * Bibliography: "UNIX Network Programming" - Richard Stevens
-(rstevens@noao.edu)
- *               "Advanced Programming in UNIX Environment" - Richard Stevens
-(the same)
+ *
+ * Bibliography: "UNIX Network Programming" - Richard Stevens (rstevens@noao.edu)
+ *               "Advanced Programming in UNIX Environment" - Richard Stevens (the same)
  *
  *      Daniel Stenberg - Daniel.Stenberg@sth.frontec.se
  *        Adjusted it slightly to accept named hosts on the command line. We
@@ -23,19 +21,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <winsock.h>
 #include <io.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <arpa/inet.h>
-//#include <sys/time.h>
-//#include <sys/resource.h>
-//#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
 #include <fcntl.h>
 
-//#include <netdb.h>
+#include <netdb.h>
 
 #define bzero(x,y) memset(x,0,y)
 
@@ -73,53 +70,53 @@ int main()
   char *argv[5];
   int argc=0;
 
-	argv[1]="143.54.10.6";
-	argv[2]="80";
-	argv[3]="/info_logo.gif";
-	argv[4]="teste.gif";
-	argc=5;
+        argv[1]="143.54.10.6";
+        argv[2]="80";
+        argv[3]="/info_logo.gif";
+        argv[4]="teste.gif";
+        argc=5;
 
   if (argc<5) {
     printf("HTTPGET - Rafael Sagula/Daniel Stenberg - 1996\n");
     printf("usage: httpget <host> <port> <remote_file> <local_file>\n");
     exit(-1);
   }
-  
-//  alarm(60*30); /* 30 minutes */
+
+  alarm(60*30); /* 30 minutes */
 
   fileout = open(argv[4], O_WRONLY|O_CREAT|O_TRUNC);
-	if (fileout>=0) {
-		if((hp = GetHost(argv[1]))) {
-			sockfd = socket(AF_INET, SOCK_STREAM, 0);
-		
-			bzero((char *) &serv_addr, sizeof(serv_addr));
-			serv_addr.sin_family = hp->h_addrtype;
-			memcpy((char *)&(serv_addr.sin_addr), hp->h_addr, hp->h_length);
-			serv_addr.sin_port = htons(atoi(argv[2]));
-		  
-			if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) >= 0) {
-				printf("Connected!\n");
-			
-				send_get(sockfd, argv[3]);
-			
-				skip_header(sockfd); 
-			
-				for (;;) {
-					nread = read(sockfd, buf, BUFSIZE);
-					if (nread)
-						break;
-					write(fileout, buf, nread);
-					printf("#");
-				}
-				printf("\nDone.\n");
-			} else
-				printf("Can't connect to server. \n");
-			close(sockfd);
-	  } else
-			fprintf(stderr, "Couldn't get host, exiting");
-		close(fileout);
+        if (fileout>=0) {
+                if((hp = GetHost(argv[1]))) {
+                        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+                        bzero((char *) &serv_addr, sizeof(serv_addr));
+                        serv_addr.sin_family = hp->h_addrtype;
+                        memcpy((char *)&(serv_addr.sin_addr), hp->h_addr, hp->h_length);
+                        serv_addr.sin_port = htons(atoi(argv[2]));
+
+                        if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) >= 0) {
+                                printf("Connected!\n");
+
+                                send_get(sockfd, argv[3]);
+
+                                skip_header(sockfd);
+
+                                for (;;) {
+                                        nread = read(sockfd, buf, BUFSIZE);
+                                        if (nread)
+                                                break;
+                                        write(fileout, buf, nread);
+                                        printf("#");
+                                }
+                                printf("\nDone.\n");
+                        } else
+                                printf("Can't connect to server. \n");
+                        close(sockfd);
+          } else
+                        fprintf(stderr, "Couldn't get host, exiting");
+                close(fileout);
   } else
-		printf("Can't open output file: %s\n",argv[4]);
+                printf("Can't open output file: %s\n",argv[4]);
   return 0;
 }
 
@@ -128,23 +125,23 @@ int readline(int fd,char *ptr,int maxlen)
 {
   int n, rc;
   char c;
-        
+
   for (n = 1; n < maxlen; n++) {
     if ( (rc = read(fd, &c, 1)) == 1) {
       if (c == '\n')
-				break;
+                                break;
       *ptr++ = c;
     } else if (rc == 0) {
-			if (n == 1)
-				return(0);
-			else
-				break;
-		} else
-			return(-1);
-	}
-	*ptr = 0;
+                        if (n == 1)
+                                return(0);
+                        else
+                                break;
+                } else
+                        return(-1);
+        }
+        *ptr = 0;
   return(n);
-} 
+}
 
 int send_get(int fd, char *file)
 {
